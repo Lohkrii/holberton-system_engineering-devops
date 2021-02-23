e673693f1e4507ec57c7b21ad5865c056ae8227c
Lohkrii <joshualopez0429@gmail.com>
2021-02-23 00:39:20 +0000

    2-repetition_token_1.rb: Simplified code" su

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # This function takes an argument and parses the specified pattern
 # The pattern is: only unique characters
-puts ARGV[0].scan(/(h(b|t)(t|n)n?)/).join
+puts ARGV[0].scan(/hb?tn/).join
