ae64878124e47aac48833de0adef7884ef994212
Lohkrii <joshualopez0429@gmail.com>
2021-04-04 14:39:51 +0000

    Fixed spacing

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Creates a file in /tmp with specifications
 file { 'holberton':
-	ensure  => file,
-	path    => '/tmp/holberton',
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love puppet'
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love puppet'
 }
